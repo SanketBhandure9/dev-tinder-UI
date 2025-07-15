96b41e07d1e2dd240c2ce9fac7ec18c6185cdcc5
SanketBhandure9 <sanketbhandure@outlook.com>
2025-07-15 15:03:43 +0530

    Build the user card

---
+++ src/components/Feed.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { USER_FEED_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
+
+const Feed = () => {
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const response = await axios.get(USER_FEED_URL, {
+        withCredentials: true,
+      });
+
+      dispatch(addFeed(response.data));
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed.data[0]} />
+      </div>
+    )
+  );
+};
+
+export default Feed;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const LOGIN_URL = "http://localhost:3000/login";
 export const LOGOUT_URL = "http://localhost:3000/logout";
+export const USER_FEED_URL = "http://localhost:3000/user/feed";
 export const PROFILE_VIEW_URL = "http://localhost:3000/profile/view";
