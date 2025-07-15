dd17547f7f87aa7776cfc1bf8adca5afaa0e0beb
SanketBhandure9 <sanketbhandure@outlook.com>
2025-07-15 13:47:40 +0530

    Login functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { PROFILE_VIEW_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(PROFILE_VIEW_URL, {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
 export const LOGIN_URL = "http://localhost:3000/login";
+export const PROFILE_VIEW_URL = "http://localhost:3000/profile/view";
